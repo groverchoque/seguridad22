bff42ad1b8d94661c76f6f59fa334cc5cdea8ef5
cachogro <groverchoque75@gmail.com>
2019-06-27 00:15:18 -0400

    funcional 0.3

---
+++ app/src/main/java/com/example/seguridad22/registrar_producto.java
@@ -0,0 +1,13 @@
+package com.example.seguridad22;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class registrar_producto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_producto);
+    }
+}
